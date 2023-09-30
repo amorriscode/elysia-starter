a7d6db9c6110bf747e26cfc1209fd765757ec50e
Anthony Morris <amorriscode@gmail.com>
2023-09-30 11:16:43 -0700

    chore: utilize port env variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .listen(process.env.PORT ?? 3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
