fccda785dd1275a8974e5458482e6131318847b7
Anthony Morris <amorriscode@gmail.com>
2025-08-31 15:32:36 -0700

    feat: update all deps and replace prettier (#3)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
-export * from '@prisma/client';
+export * from "@prisma/client";
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Elysia } from 'elysia';
-import { users } from '~/resources/users/router';
+import { Elysia } from "elysia";
+import { users } from "~/resources/users/router";
 
 const app = new Elysia()
-  .get('/', () => 'Eat your vegetables 🥦')
-  .use(users)
-  .listen(process.env.PORT ?? 3000);
+	.get("/", () => "Eat your vegetables 🥦")
+	.use(users)
+	.listen(process.env.PORT ?? 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
